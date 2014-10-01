b01cdbb90021d8906aac39b8f2c72803edc65e00
March <tpatikorn@hotmail.com>
2014-10-01 14:15:07 -0400

    count_files now use pygit2.clone_repository instead of os.system.

--- repoManager/repo_mgmt.py
+++ repoManager/repo_mgmt.py
@@ -1,13 +1,15 @@
 import os
 import subprocess
+from pygit2 import Repository, clone_repository
 
 class GitRepo(object):
     def __init__(self, url):
         self.url = url
 
     def count_files(self):
-        os.system('mkdir repo')
-        os.system('git clone ' + self.url + ' repo')
+        repo_url = self.url
+        repo_path = 'repo'
+        repo = clone_repository(url = repo_url, path = repo_path)
         num_files = subprocess.check_output('cd repo && git ls-files | wc -l', shell = True)
         os.system('rm -rf repo')
         return num_files
