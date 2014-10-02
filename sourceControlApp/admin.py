255e0e24a249bddc3c7a99dc93a90547fb22630d
Anjali Venkatesh <avenkatesh@wpi.edu>
2014-10-02 03:05:50 -0400

    Actually displays the repo name now. Also error checking on add repo, minor details refactoring

--- sourceControlApp/admin.py
+++ sourceControlApp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from sourceControlApp.models import GitStore, CodeAuthor
 
 class GitStoreAdmin(admin.ModelAdmin):
-    list_display = ["gitRepositoryURL", "numFiles"]
+    list_display = ["gitRepositoryURL", "numFiles", "repoName"]
 
 class CodeAuthorAdmin(admin.ModelAdmin):
     list_display = ["name", "repository", "num_commits"]
