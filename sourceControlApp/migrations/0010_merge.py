1b948d58f91bf64bcfbf830471edc70a8bd32130
March <tpatikorn@hotmail.com>
2014-10-08 22:09:38 -0400

    commit migrations.

---
+++ sourceControlApp/migrations/0010_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sourceControlApp', '0007_auto_20141008_1948'),
+        ('sourceControlApp', '0009_commit_num_patches'),
+    ]
+
+    operations = [
+    ]
