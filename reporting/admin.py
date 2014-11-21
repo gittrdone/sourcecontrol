b7f1bc516f442efb2aaf980a8bf144be4e3542ab
Anjali Venkatesh <avenkatesh@wpi.edu>
2014-11-21 16:45:49 -0500

    Can see reports and queries in admin mode

--- reporting/admin.py
+++ reporting/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from reporting.models import Query, Report
 
 # Register your models here.
+
+
+class QueryAdmin(admin.ModelAdmin):
+    list_display = ["name", "query_command", "user"]
+
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "repo", "user"]
+
+# Register your models here.
+admin.site.register(Query, QueryAdmin)
+admin.site.register(Report, ReportAdmin)
