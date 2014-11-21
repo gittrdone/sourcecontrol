6f1dad6ebf295bb34d566992e1255e0098e25883
Anjali Venkatesh <avenkatesh@wpi.edu>
2014-11-21 14:11:02 -0500

    Commit chart whoo

--- charting/urls.py
+++ charting/urls.py
@@ -5,5 +5,5 @@ from charting import views
 urlpatterns = patterns('',
     url(r'^repo_detail/(?P<repo_id>[0-9]+)', views.json_pie_data_for_repo, name='repo_detail_pie'),
     url(r'^repo_detail_day/(?P<repo_id>[0-9]+)', views.json_bar_data_for_repo_commits, name='repo_detail_bar_day'),
-
+    url(r'^user_detail_commits/(?P<committer_id>[0-9]+)', views.json_bar_data_for_user_repo_commits, name='user_repo_detail_bar_day'),
 )
