cda556b6cdcd5b99398eb0edb7b813e72b45b044
Anjali Venkatesh <avenkatesh@wpi.edu>
2014-12-08 14:36:55 -0500

    Login check in processors.py

--- sourcecontrol/processors.py
+++ sourcecontrol/processors.py
@@ -1,13 +1,14 @@
 def repo_nav_options(request):
     user = request.user
-    repos = user.sourcecontroluser.ownedRepos.all()
-    default_branch_ids = []
-    default_branch_files = []
-    for repo in repos:
-        if repo.git_repo.status == 3:
-            default_branch_files.append(repo.git_repo.branches.all()[0].num_files)
-            default_branch_ids.append(repo.git_repo.branches.all()[0].pk)
-        else:
-            default_branch_files.append(-1)
-            default_branch_ids.append(-1)
-    return {'repo_nav_options':zip(repos, default_branch_ids, default_branch_files)}
+    if request.user.is_authenticated():
+        repos = user.sourcecontroluser.ownedRepos.all()
+        default_branch_ids = []
+        default_branch_files = []
+        for repo in repos:
+            if repo.git_repo.status == 3:
+                default_branch_files.append(repo.git_repo.branches.all()[0].num_files)
+                default_branch_ids.append(repo.git_repo.branches.all()[0].pk)
+            else:
+                default_branch_files.append(-1)
+                default_branch_ids.append(-1)
+        return {'repo_nav_options':zip(repos, default_branch_ids, default_branch_files)}
