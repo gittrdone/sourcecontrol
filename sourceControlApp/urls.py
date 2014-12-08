61ec4a79cbd082f70d4c67eda5e4275eec983df5
Justin Chines <justin.chines@gmail.com>
2014-12-08 04:10:30 -0500

    Add help page with links

--- sourceControlApp/urls.py
+++ sourceControlApp/urls.py
@@ -18,5 +18,7 @@ urlpatterns = patterns('',
     url(r'^login', views.logon, name='login'),
     url(r'^do_login', views.do_logon, name='do_login'),
 
+    url(r'^queryhelp', views.queryhelp, name='query_help'),
+
     url(r'^logout', views.do_logout, name='logout'),
 )
